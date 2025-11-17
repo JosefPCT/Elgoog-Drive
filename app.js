d2ae93b23dd9ddbcc050f16251cc0e34164aa41f
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-11-17 18:21:22 +0800

    feat: implement sessions with session-store and prisma-session-store

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express');
+const expressSession = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const pclient = require('./db/client');
 
 const indexRouter = require('./routes/index'); 
 
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT | 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(
+    expressSession({
+        cookie: {
+            maxAge: 1 * 24 * 60 * 60 * 1000 // 1 day
+        },
+        secret: process.env.SECRET,
+        resave: true,
+        saveUninitialized: true,
+        store: new PrismaSessionStore(
+            pclient,
+            {
+                checkPeriod: 2 * 60 * 10,
+                dbRecordIdIsSessionId: true,
+                dbRecordIdFunction: undefined,
+            }
+        )
+
+    })
+)
 
 app.use('/', indexRouter);
 
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,18 @@
-const prisma  = require('../db/client');
+const prisma = require("../db/client");
 
+module.exports.indexGetRoute = async (req, res, next) => {
+  // const allusers = await prisma.user.findMany();
+  // res.send(allusers);
 
-module.exports.indexGetRoute = async(req, res, next) => {
-    const allusers = await prisma.user.findMany();
-    res.send(allusers);
+  // Testing out sessions
+//   if (req.session.viewCount) {
+//     req.session.viewCount = req.session.viewCount + 1;
+//   } else {
+//     req.session.viewCount = 1;
+//   }
+//   console.log(req.session);
 
-    res.render('index', {
-    title: 'Home'
+  res.render("index", {
+    title: "Home",
   });
-}
+};
