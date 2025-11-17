0f7791f0f0ba69cff0dd96951ad423c2ba83b4e8
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-11-17 17:41:49 +0800

    fix: refactor prisma client to its own file

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,9 @@
-const { PrismaClient } = require('../generated/prisma')
-
-const prisma = new PrismaClient();
+const prisma  = require('../db/client');
 
 
 module.exports.indexGetRoute = async(req, res, next) => {
-    // const allusers = await prisma.user.findMany();
-    // res.send(allusers);
+    const allusers = await prisma.user.findMany();
+    res.send(allusers);
 
     res.render('index', {
     title: 'Home'
