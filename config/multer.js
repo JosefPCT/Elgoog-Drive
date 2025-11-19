e157d06fac2e1cc7d643f58e496486f207abeca2
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-11-19 12:21:36 +0800

    feat: add config file for multer in 'config/multer.js', set the destination to 'uploads' directory, and limit fileSize to 5mb, created unused template to filter files

---
+++ config/multer.js
@@ -0,0 +1,34 @@
+const multer = require("multer");
+
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5mb
+
+const upload = multer({
+  dest: "uploads/",
+  // fileFilter: fileFilter,
+  limits: {
+    fileSize: MAX_FILE_SIZE,
+  },
+});
+
+function fileFilter(req, file, cb) {
+  console.log("Inside filter function");
+  // Show file object without destination, filename, path, and size properties
+  console.log("req body", req.body);
+  console.log("file object", file);
+
+  // The function should call `cb` with a boolean
+  // to indicate if the file should be accepted
+  try {
+    if (file.mimetype === "image/jpeg") {
+      console.log("Gets uploaded in /uploads directory...");
+      cb(null, true);
+    } else {
+      console.log("Does not get uploaded");
+      cb(null, false);
+    }
+  } catch (error) {
+    cb(new Error("I don\t have a clue!"));
+  }
+}
+
+module.exports = upload;
