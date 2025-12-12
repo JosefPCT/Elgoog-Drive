931e35cbe41b1477234e1b77cefad764a0ae29ff
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-12-12 15:31:44 +0800

    feat: programatically upload files to cloudinary with multer's memory storage

---
+++ config/cloudinary.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
+
+// import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: 'dnonrqcmf',
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+module.exports = cloudinary;
--- controllers/fileController.js
+++ controllers/fileController.js
@@ -3,6 +3,7 @@ const { body, validationResult, matchedData } = require("express-validator");
 const queries = require("../db/queries");
 const { isAuth } = require("./utils/authMiddleware");
 const upload = require("../config/multer");
+const cloudinary = require('../config/cloudinary');
 
 // Post Route
 
@@ -12,7 +13,26 @@ module.exports.fileNewPostRoute = [
     console.log("Uploading a new file...");
     console.log(req.body);
     console.log("File object", req.file);
-    res.send("aa");
+
+    if(!req.file){
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    // Cloudinary stuff
+
+    cloudinary.uploader.upload_stream({ resource_type: 'auto'}, (error, result) => {
+      if(error) {
+        console.log(error);
+        return res.status(500).json({ error: 'Error uploading to Cloudinary'});
+      }
+      console.log('Uploaded to Cloudinary succesfully!');
+      console.log('Results: ', result);
+      console.log('Public_id: ', result.public_id);
+      console.log('Secure Url: ', result.secure_url);
+      res.json({ public_id: result.public_id, url: result.secure_url });
+    }).end(req.file.buffer);
+
+    // res.send("aa");
   },
 ];
 
