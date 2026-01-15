5e16ef662df25e4f63ff6b92369f7589b6ad7c25
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-01-15 17:16:38 +0800

    feat: add post route for /share/:id/folder/:folderId for sorting folders and files

--- controllers/sharecontroller.js
+++ controllers/sharecontroller.js
@@ -16,6 +16,53 @@ module.exports.newSharePostRoute = async (req, res, next) => {
   res.redirect(`${previousUrl}?sharing=${data.id}`);
 }
 
+module.exports.shareFolderIdPostRoute = async(req, res, next) => {
+  let shareData = await queries.findShareDataById(req.params.id);
+  let createdAt = shareData.createdAt;
+
+  const differenceInMilliSeconds = Date.now() - createdAt;
+
+  const oneSecond = 1000;
+  const oneMinute = oneSecond * 60;
+  const oneHour = oneMinute * 60;
+  const oneDay = oneHour * 24;
+
+  const differenceInSeconds = differenceInMilliSeconds / oneSecond;
+  const diffInMins = differenceInMilliSeconds / oneMinute;
+  const diffInHours = differenceInMilliSeconds / oneHour;
+  const diffInDays = differenceInMilliSeconds / oneDay;
+
+  if(shareData.expiry > diffInDays){
+    console.log("Link not expired");
+
+    let sortOrder = ( req.body.isAsc === "true" ) ? "false" : "true";
+    console.log(sortOrder);
+    let folder;
+    if(req.body._method === 'SORT'){
+      folder = await queries.getFolderById(parseInt(req.params.folderId), req.body.colName, req.body.isAsc);
+    } else {
+      folder = await queries.getFolderById(parseInt(req.params.folderId));
+    }
+    let data = [...folder.subfolders, ...folder.files];
+
+    // const user = { firstName: 'unknown', lastName: 'unknown' };
+    const user = await queries.getCurrentUserById(folder.userId);
+
+    res.render('pages/share/shareFolderId', {
+    title: 'Shared Folder',
+    shareId: req.params.id,
+    folderId: req.params.folderId,
+    data: data,
+    // urlWithoutQuery: 'a',
+    sortOrder: sortOrder,
+    user: user
+  })
+  } else {
+    console.log("Link Expired");
+    res.send("Link has expired");
+  }
+}
+
 // GET Route
 
 
--- routes/shareRoute.js
+++ routes/shareRoute.js
@@ -9,7 +9,9 @@ router.post('/new', controller.newSharePostRoute);
 // GET Route
 
 router.get('/:id', controller.shareIdGetRoute);
+
 router.get('/:id/folder/:folderId', controller.shareFolderIdGetRoute);
+router.post('/:id/folder/:folderId', controller.shareFolderIdPostRoute);
 
 router.get('/:id/file/:fileId', controller.shareFileIdGetRoute);
 
